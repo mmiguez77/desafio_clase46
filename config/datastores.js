bcef8eb205513c2f40b0b09a8ef4fd0b2aad8b5d
mmiguez77 <martinmiguez77@gmail.com>
2021-09-06 17:18:18 -0300

    Desafio para corregir

--- config/datastores.js
+++ config/datastores.js
@@ -48,8 +48,8 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-mongo',
+    url: 'mongodb+srv://proyecto:coder@cluster0.tqtau.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 
   },
 
